b913f05255d436512affd80cfb4829d3b706b3c1
qtu-UBC <qingshi.tu@ubc.ca>
2024-02-29 14:22:08 -0800

    2nd push

---
+++ post-processing.py
@@ -0,0 +1,129 @@
+"""
+This script processes the extracted elements (from pdfs)
+
+@Qingshi Tu
+"""
+
+
+"""
+Import libraries
+"""
+import openai
+import langchain_openai
+from langchain.prompts import PromptTemplate
+import os
+import glob
+import base64
+import pandas as pd
+
+
+"""
+Config
+"""
+# path to the extracted elements
+# same folder to store the post-processed elements
+output_folder = "/home/bizon/Documents/code_projects/tiangong_ai_unstructured_dev/output"
+
+vision_client = openai.OpenAI()
+llm = langchain_openai.OpenAI(temperature=0)
+
+"""
+Define functions
+"""
+def vision_completion(image_path: str) -> str:
+    """
+    This function is adapted from TianGong-AI-Unstructure > src > tools > vision.py
+    """
+    # Open the image file in binary mode
+    with open(image_path, 'rb') as image_file:
+        # Convert the binary data to base64
+        image_data = base64.b64encode(image_file.read()).decode("utf-8")
+    
+    try:
+        response = vision_client.chat.completions.create(
+            model="gpt-4-vision-preview",
+            messages=[
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": "What is in this image? Only return neat facts in English.",
+                        },
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": f"data:image/jpeg;base64,{image_data}",
+                            },
+                        },
+                    ],
+                }
+            ],
+            max_tokens=300,
+        )
+        return response.choices[0].message.content
+    except Exception as e:
+        print(f"Error analyzing image: {e}")
+        return None
+
+
+def image_screening(image_path: str) -> str:
+    """
+    (1) use gpt-4 vision to generate a summary of the image
+    (2) then ask it to decide whether the image belongs to one of the following groups:
+        - system boundary chart of an life cycle assessment study
+        - impact assessment results of an life cycle assessment study
+    (3) remove the image if it does not belong any of the abovementioned groups
+    """
+
+    # [step 1] use gpt-4 vision to generate a summary of the image
+    image_summary = vision_completion(image_path)
+    # print(image_summary)
+
+    # [step 2] decide whether or not the image belongs to the expected groups
+    # define the template with explicit input variables
+    template_string = """
+        Given the context and specific details provided, please answer the following question.
+
+        Context: In a life cycle assessment study, there two types of figures:  (1) system boudary diagram: visually delineates the processes and flows to be included in the LCA, marking the limits of the analysis; 
+        (2) impact assessment results: visually represents the outcomes of the environmental impacts assessed, illustrating the magnitude and distribution of impacts across different categories.
+
+        Question: based on the content of "{image_summary}", decide whether it is describing a system boundry diagram, an impact assessment results diagram, or neither. Output your answer
+
+        Answer: """
+
+    prompt_template = PromptTemplate(
+        template=template_string,
+        input_variables=["image_summary"]
+    )
+
+    # make the query to decide whether or not the image belongs to the expected groups
+    query = prompt_template.template.format(image_summary=image_summary)
+    return llm(query)
+
+
+
+if __name__ == "__main__":
+    # get a list of image paths
+    pattern = os.path.sep.join([output_folder, '*.jpg'])
+    image_path_list = glob.glob(pattern)
+    # for image_path in image_path_list:
+    #     print(image_path)
+
+    # loop over the paths and retain only the ones relevant to LCA
+    decision_dict = {}
+
+    for image_path in image_path_list:
+        decision_dict[image_path.split("/")[-1]] = image_screening(image_path)
+
+    # convert dict to df
+    data = list(decision_dict.items())
+    decision_df = pd.DataFrame(data, columns=['ImagePath', 'Decision'])
+    print(decision_df)
+
+    # save the results in csv to the output folder
+    decision_df.to_csv(os.path.sep.join([output_folder,'image_screening_results.csv']), index=False)
+
+  
+
+
---
+++ test_chunk_by_title_pdf.py
@@ -0,0 +1,153 @@
+# import tempfile
+from unstructured.chunking.title import chunk_by_title
+from unstructured.cleaners.core import (
+    clean,
+    group_broken_paragraphs,
+)
+from unstructured.documents.elements import Footer, Header, Image, CompositeElement, Table
+from unstructured.partition.auto import partition
+from tools.vision import vision_completion
+import pandas as pd
+import re
+from typing import List
+from io import StringIO
+import os
+
+"""
+Define helper functions
+"""
+# function to check if a string contains HTML tags
+def contains_html(text) -> bool:
+    # Regular expression pattern to detect typical HTML tags
+    html_pattern = re.compile('<.*?>')
+    return bool(html_pattern.search(text))
+
+# function to convert a table from html format to dataframe
+def html_to_table(html_string: str) -> pd.DataFrame:
+    """
+    Converts an HTML string with table data to a pandas DataFrame.
+
+    :param html_string: String containing HTML data.
+    :return: A pandas DataFrame
+    """
+    try:
+        # Parse HTML string and convert tables to DataFrames
+        table = pd.read_html(StringIO(html_string))[0] # pd.read_html reads HTML tables into a list of DataFrame objects. 
+                                                       # But in this case, we will only have one table per html_string
+
+        return table
+    except ValueError as e:
+        # Return an empty list if no tables are found or if an error occurs
+        print(f"No tables found or error in parsing: {e}")
+        return []
+
+"""
+Config information
+"""
+input_folder = "/home/bizon/Documents/code_projects/tiangong_ai_unstructured_dev/input"
+output_folder = "/home/bizon/Documents/code_projects/tiangong_ai_unstructured_dev/output"
+pdf_name = os.path.sep.join([input_folder,"CtoG-LCA-Canadian-CLT.pdf"])
+
+
+"""
+Code for extraction
+"""
+min_image_width = 250
+min_image_height = 270
+
+elements = partition(
+    filename=pdf_name,
+    header_footer=False,
+    pdf_extract_images=True,
+    pdf_image_output_dir_path=output_folder, 
+    skip_infer_table_types=["jpg", "png", "xls", "xlsx"],
+    strategy="hi_res",
+    hi_res_model_name="yolox",
+)
+
+filtered_elements = [
+    element
+    for element in elements
+    if not (isinstance(element, Header) or isinstance(element, Footer))
+]
+
+for element in filtered_elements:
+    if element.text != "":
+        element.text = group_broken_paragraphs(element.text)
+        element.text = clean(
+            element.text,
+            bullets=False,
+            extra_whitespace=True,
+            dashes=False,
+            trailing_punctuation=False,
+        )
+    elif isinstance(element, Image):
+        point1 = element.metadata.coordinates.points[0]
+        point2 = element.metadata.coordinates.points[2]
+        width = abs(point2[0] - point1[0])
+        height = abs(point2[1] - point1[1])
+        if width >= min_image_width and height >= min_image_height:
+            # generate text from the image
+            element.text = vision_completion(element.metadata.image_path)
+
+chunks = chunk_by_title(
+    elements=filtered_elements,
+    multipage_sections=True,
+    combine_text_under_n_chars=100,
+    new_after_n_chars=512,
+    max_characters=4096,
+)
+
+text_list = []
+for chunk in chunks:
+    if isinstance(chunk, CompositeElement):
+        text = chunk.text
+        text_list.append(text)
+    elif isinstance(chunk, Table):
+        if text_list:
+            text_list[-1] = text_list[-1] + "\n" + chunk.metadata.text_as_html
+        else:
+            text_list.append(chunk.metadata.text_as_html)
+result_list = []
+
+for text in text_list:
+    split_text = text.split("\n\n", 1)
+    if len(split_text) == 2:
+        title, body = split_text
+    else:
+        # Handle cases where the split does not result in two parts
+        result_list.append({'title': 'Unknown', 'body': text})
+
+# for result in result_list:
+#     print(result)
+#     print("\n\n" + "-" * 80)
+#     input()
+
+# Convert the result_list to a DataFrame
+df = pd.DataFrame(result_list)
+# print(df)
+
+# identify the cells with html tags (i.e., extracted tables)
+# apply the function to each row in the 'body' column and add a new column 'contains_html'
+df['contains_html'] = df['body'].apply(contains_html)
+
+# Display the modified DataFrame
+print(df)
+
+# reformat the tables (from html to dataframe)
+df_table_only = df[df['contains_html']==True] 
+table_dfs = df_table_only['body'].apply(html_to_table)
+
+# Export the tables into an xlsx file
+with pd.ExcelWriter(os.path.sep.join([output_folder,'output_tables.xlsx'])) as writer:
+    for i,table_df in enumerate(table_dfs):
+        table_df.to_excel(writer, sheet_name=f"extracted_tab_{i}")
+
+
+"""
+Code for post-processing
+"""
+# retain only relevant images
+
+
+# retain only inventory tables
